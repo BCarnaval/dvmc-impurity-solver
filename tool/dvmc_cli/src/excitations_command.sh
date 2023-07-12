fda19f1588d041225f00d55d40d60dfe53577ff8
BCarnaval <antoinedelagrave@hotmail.com>
2023-07-12 11:39:23 -0400

    Updated dVMC CLI

--- tool/dvmc_cli/src/excitations_command.sh
+++ tool/dvmc_cli/src/excitations_command.sh
@@ -3,15 +3,21 @@
 excitations () {
   # Constants
   N_PROC="${DVMC_MPI_PROC}"
-  OPTIMIZED="${args[optimized]}"
-  NAMELIST="${args[namelist]}"
+  NAMELIST_G=$(find . -name "namelist_G.def" -type f)
+  OPTIMIZED=$(find ./output -name "zqp_opt.dat" -type f)
 
   # Script
-  if [ $(basename "${NAMELIST}") == 'namelist_G.def' ] && [ -f "${NAMELIST}" ]; then
+  if [[ -f "${NAMELIST_G}" ]] && [[ -f "${OPTIMIZED}" ]]; then
     green "[@] Beginning dynamical VMC calculations..."
-    mpirun -n "${N_PROC}" "${DVMC_SCRIPTS_LOCATION}"/dvmc.out "${NAMELIST}" "${OPTIMIZED}"
+    mpirun -n "${N_PROC}" "${DVMC_SCRIPTS_LOCATION}"/dvmc.out "${NAMELIST_G}" "${OPTIMIZED}"
+  elif [[ ! -f "${NAMELIST_G}" ]]; then
+    red "[X] File named: 'namelist_G.def' not found in current directory."
+    exit 1
+  elif [[ ! -f "${OPTIMIZED}" ]]; then
+    red "[X] File named: './output/zqp_opt.dat' not found in current directory."
+    exit 1
   else
-    red "[X] Input must be a file named: 'namelist_G.def'."
+    red "[X] Neither './output/zqp_opt.dat' and 'namelist_G.def' have been found."
     exit 1
   fi
 }
--- tool/dvmc_cli/src/process_output_command.sh
+++ tool/dvmc_cli/src/process_output_command.sh
@@ -1,7 +1,10 @@
 # Process-output command script
 
 process_output () {
+  # Constants
   PREFIX="${args[prefix]}"
+
+  # Script
   green "[@] Processing ${PREFIX}*bin files..."
   "${DVMC_SCRIPTS_LOCATION}"/mergeOutputBin.py "${PREFIX}"*bin
 }
